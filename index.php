008aecb16f51cac9d19d28558c83a3d627e7debb
D4CPOKER <davicamillo2009@gmail.com>
2025-05-29 16:54:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,31 +6,37 @@
   <meta charset="UTF-8"> <!-- Suporte a caracteres especiais -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Responsividade em dispositivos móveis -->
   <title>Menu</title> <!-- Título da aba -->
-  <link rel="icon" type="image/png" href="img/l.png">
+  <link rel="icon" type="image/png" href="img/caveira1.png">
+   <source src="../vid/fundo.mp4" type="video/mp4" />
 
   <!-- Link para CSS externo (você precisa definir o nome do arquivo corretamente) -->
   <link rel="stylesheet" href="css/.css">
 
   <!-- Fonte do Google (Uncial Antiqua) -->
-  <link href="https://fonts.googleapis.com/css2?family=Uncial+Antiqua&display=swap" rel="stylesheet">
+
 </head>
 
 <body >
+<video autoplay muted loop playsinline id="video-fundo">
+  <source src="vid/fundo.mp4" type="video/mp4" />
+  Seu navegador não suporta vídeo HTML5.
+</video>
   <div class="Topo"> <!-- Cabeçalho principal -->
-    <img class="img" src="img/l.png" alt="Logo"> <!-- Logotipo no topo -->
+    <img class="img" src="img/logo_com_titulo.png" alt="Logo"> <!-- Logotipo no topo -->
 <div class="menu">
   <!-- Itens principais do menu -->
   <nav class="menu-links">
-    <a class="menu_item" href="historia.php">HISTÓRIA</a>
-    <a class="menu_item" href="jogar.php">COMO JOGAR</a>
-    <a class="menu_item" href="inventario.php">INVENTÁRIO</a>
-    <a class="menu_item" href="apoie.php">APOIE</a>
-    <a class="menu_item" href="suporte.php">SUPORTE</a>
+    <a class="menu_item" href="#">HISTÓRIA</a>
+    <a class="menu_item" href="#">COMO JOGAR</a>
+    <a class="menu_item" href="#">INVENTÁRIO</a>
+    <a class="menu_item" href="#">APOIE</a>
+    <a class="menu_item" href="#">SUPORTE</a>
   </nav>
+  </div>
       <?php if (empty($_SESSION['login_sucesso'])): ?>
         <!-- Caso o usuário NÃO esteja logado, mostra opções de login e cadastro -->
         <a class="menu_l" href="login.php"><div class="b">Login</div></a>
-        <a class="menu_l" href="cadastro.php"><div class="b">Cadastrar</div></a>
+        <a class="menu_l" href="cadastro.php"><div class="b">Cadastre-se</div></a>
 
       <?php else: ?>
 <div class="login-wrapper">
@@ -56,10 +62,10 @@
 <footer>
   <div class="rodape"> <!-- Rodapé da página -->
 
-    <p>
-      <a href="#">Política de Privacidade</a> |
-      <a href="#">Termos de Uso</a> |
-      <a href="#">Contato</a>
+    <p >
+      <a href="#" class="link_rodape">Política de Privacidade</a> |
+      <a href="#" class="link_rodape">Termos de Uso</a> |
+      <a href="#"class="link_rodape">Contato</a>
     </p>
 	<p>
       &copy; 2025 RPG Câmara da Caveira. Todos os direitos reservados por
